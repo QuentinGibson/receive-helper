fba6cacc03ff9423114a2d1572b50cf1a1f4d4dc
Quentin Gibson <qgibson3@students.kennesaw.edu>
2021-05-03 04:11:58 -0400

    create template app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import List from './components/List'
+import Form from './components/Form'
+import Footer from './components/Footer/Footer'
+import aftlite from './utils/aftlite.js'
 import './App.css';
 
+type item = {
+  asin: string;
+  location: string;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [palletOrder, setPalletOrder] = useState('')
+  const [palletID, setPalletID] = useState('')
+  let itemsDefault: Array<item> = []
+  const [items, setItems] = useState(itemsDefault)
+
+  useEffect(() => {
+    if (palletOrder !== '' && palletID !== '') {
+      const items = aftlite.getPO(palletOrder, palletID)
+      setItems(items)
+    }
+  }, [palletOrder, palletID])
+
+    if (items.length === 0) {
+      return (
+        <Form
+          setPalletOrder={setPalletOrder}
+          setPalletID={setPalletID}
+        />
+      )
+    } else {
+      return (
+        <div>
+          <List
+            items={items}
+          />
+          <Footer/>
+        </div>
+      )
+    }
 }
 
 export default App;
